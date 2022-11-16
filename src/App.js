55e861148f6531e4536c03ef638ae0f5b81b54c3
alaamohamed58 <alaamohamed58999@gmail.com>
2022-11-16 09:45:02 +0000

    final

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import AllPages from "./pages/AllPages";
 import { CookiesProvider } from "react-cookie";
 import { AuthContextProvider } from "./context/auth-context";
 
-window.domain = "http://miner.testingdigiquag.fun/api/v1/";
+window.domain = "https://miner.testingdigiquag.fun/api/v1/";
+// "http://miner.testingdigiquag.fun/api/v1/"
+
 function App() {
   return (
     <AuthContextProvider>
